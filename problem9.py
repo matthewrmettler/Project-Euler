eea14bce40755d22321eac83db47d3f43642b95f
matthewrmettler <matthewrmettler@gmail.com>
2014-06-14 04:13:00 -0400

    Problem 9 Done

--- problem9.py
+++ problem9.py
@@ -10,6 +10,18 @@ a^2 + b^2 = c^2
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 
-Status: Incomplete
+Status: Correct
 '''
 import math
+
+def isTriplet(a, b, c):
+	return (math.pow(a,2) + math.pow(b,2) == math.pow(c, 2))
+
+def sumToThousand(a, b, c):
+	return (a + b + c == 1000)
+
+for i in range(1, 1000):
+	for j in range(1000-i, i, -1):
+			k = int(math.sqrt(math.pow(i, 2) + math.pow(j, 2)))
+			if sumToThousand(i, j, k) and isTriplet(i, j, k):
+				print(str(i*j*k))
