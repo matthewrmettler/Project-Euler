e1ca0b979bc6518185a1aae780ebd9124a2ca9e9
matthewrmettler <matthewrmettler@gmail.com>
2014-06-14 03:03:17 -0400

    Problem 5 complete

--- problem5.py
+++ problem5.py
@@ -6,6 +6,19 @@ Project Euler, Problem 5
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-Status: Incomplete
+Status: Correct
 '''
 import math
+
+number = 20
+
+def divisible(number):
+	for i in range(1, 20):
+		if number % i != 0:
+			return False
+	return True
+
+while(not divisible(number)):
+	number += 20
+
+print(number)
---
+++ problem6.py
@@ -0,0 +1,10 @@
+'''
+Author: Matthew Mettler
+Project Euler, Problem 6
+https://projecteuler.net/problem=6
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+Status: Incomplete
+'''
+import math
