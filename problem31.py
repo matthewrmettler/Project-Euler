1601e2cb73791094ab3131269d5a2f73b9f0d285
matthewrmettler <matthewrmettler@gmail.com>
2014-06-22 05:18:31 -0400

    Problem 31 completed

--- problem31.py
+++ problem31.py
@@ -3,10 +3,24 @@ Author: Matthew Mettler
 Project Euler, Problem 31
 https://projecteuler.net/problem=31
 
-How many different ways can £2 be made using any number of coins?
+How many different ways can 2 pounds be made using any number of coins?
 
-Status: Incomplete, trying to do in one line
+Status: Complete, recursive solution
 Correct according to https://code.google.com/p/projecteuler-solutions/wiki/ProjectEulerSolutions
 [Finished in 3.6s]
 '''
-print(len(set([x for x in ([x] for x in range(0,201))])))
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+coins = coins[::-1]
+init_target = 200
+
+def coinsNum(num, target):
+	#print("CoinsNum with %d, %d" % (coins[num], target))
+	count = 0
+	if target == 0 : return 1
+	if coins[num] == 1: return 1
+	ways = target / coins[num]
+	for i in range(ways, -1, -1):
+		count += coinsNum(num+1, target-i*coins[num])
+	return count
+
+print(coinsNum(0, init_target))
---
+++ problem32.py
@@ -0,0 +1,13 @@
+'''
+Author: Matthew Mettler
+Project Euler, Problem 32
+https://projecteuler.net/problem=32
+
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+
+Status: Incomplete
+Will check according to https://code.google.com/p/projecteuler-solutions/wiki/ProjectEulerSolutions
+[Finished in 3.6s]
+'''
