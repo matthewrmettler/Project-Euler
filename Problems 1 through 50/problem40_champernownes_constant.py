c2bed920c5f32dd10ed8564235fbc1c977d6ac23
Matthew Mettler <matthewrmettler@gmail.com>
2015-09-29 07:47:08 -0400

    Problem 40 completed!

--- Problems 1 through 50/problem40_champernownes_constant.py
+++ Problems 1 through 50/problem40_champernownes_constant.py
@@ -12,6 +12,12 @@ It can be seen that the 12th digit of the fractional part is 1.
 If dn represents the nth digit of the fractional part, find the value of the following expression.
 
 d1 x d10 x d100 x d1000 x d10000 x d100000 x d1000000
+Status: Correct
 '''
-from operator import mul
-print(reduce(mul,[int(10**x) for x in range(0,7)], 1))
+digits = ""
+#500,000 is arbitrary; I just needed the string to contain at least 1 million digits
+for i in range(500000):
+    digits += str(i)
+
+#Since I started the range above at 0, the indexing for the ith digit is actually starting at 1
+print(int(digits[1]) * int(digits[10]) * int(digits[100]) * int(digits[1000]) * int(digits[10000]) * int(digits[100000]) * int(digits[1000000]))
